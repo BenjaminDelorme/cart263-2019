d3cb594b1a2d4ddbb7d7d54e07ccb10b6307dc74
BenjaminDelorme <31974802+BenjaminDelorme@users.noreply.github.com>
2019-03-06 00:09:24 -0500

    First style& aesthetics
    Added a simple design, an avatar and some light that changes colors when clicked on. Added animation to make the experience more immersive.

--- project2/darkness/js/script.js
+++ project2/darkness/js/script.js
@@ -11,6 +11,7 @@ let correctGuess;
 let point;
 let guess = ["left","up","right","down"];
 let $points = $('<div class="points"></div>');
+let $ghost = $('<img class="ghost" src=""></img>');
 let voices = [
 
 
@@ -24,14 +25,14 @@ let voices = [
 "Australian Female",
 "Finnish Female",
 
-"Arabic Male"  ,
+"Arabic Male",
 
 "Czech Female",
 "Danish Female",
 
 "Indonesian Female",
 
-"Polish Female" ,
+"Polish Female",
 
 "Portuguese Female",
 
@@ -43,6 +44,10 @@ let voices = [
 ];
 let goodVoice;
 let rng;
+let $lights;
+let light1 = "assets/images/light1.png";
+let light2 = "assets/images/light2.png";
+let flickInterval;
 //stackoverflow.com/questions/3385936/jquery-follow-the-cursor-with-a-div
 
  $(document).ready(function(){
@@ -51,7 +56,9 @@ let rng;
     $up = $("#up");
     $right = $("#right");
     $down = $("#down");
+    $lights = $(".light");
    $avatar.hide();
+   $("#zones").hide();
    $("#start").hover(function(){$("#start").css({cursor: "pointer"}) });
 
    $("#start").on('click', function(){
@@ -82,13 +89,14 @@ let rng;
 
 
 function startGame(){
-  // $avatar.fadeIn();
-  // $('html').css({cursor:"none"});
+  $avatar.fadeIn();
+  $('html').css({cursor:"none"});
   point=5;
   goodVoice = voices[Math.floor(Math.random() * voices.length)];
   console.log(goodVoice);
-  update();
   newRound();
+  update();
+
 }
 
 
@@ -103,15 +111,16 @@ function update(){
   }, 20);
 
 
-  setInterval(hint,Math.floor(Math.random() * 3000));
-  // setInterval(function(){
-  // },Math.floor(Math.random() * 2000) );
 
 
+$ghost.attr("src","assets/images/ghost1.png");
+$avatar.append($ghost);
 
+
+//
 // $left.hover(function(){
-//   console.log("onLeft!");
 // });
+//
 // $up.hover(function(){
 //   console.log("onUp!");
 // });
@@ -125,29 +134,65 @@ function update(){
 
 
 
-
 $(".light").on('click',function(){
-  if($(this).text()===correctGuess){
+
+  if($(this).attr("id")===correctGuess){
       console.log('correct');
       point++;
       console.log(point);
+      clearInterval(flickInterval);
       $('body').append($points);
+        if($(this).attr("id")==="left"){
+          $(this).attr('src',"assets/images/blueLight1.png");
+          $("#zones").delay(500).animate({left: '100%'},{ duration: 500}).fadeOut().animate({left: '0%'},{ duration: 100});
+        }if($(this).attr("id")==="up"){
+          $(this).attr('src',"assets/images/blueLight1.png");
+          $("#zones").delay(500).animate({top: '100%'},{ duration: 500}).fadeOut().animate({top: '0%'},{ duration: 100});
+        }if($(this).attr("id")==="right"){
+          $(this).attr('src',"assets/images/blueLight1.png");
+          $("#zones").delay(500).animate({left: '-100%'},{ duration: 500}).fadeOut().animate({left: '0%'},{ duration: 100});
+        }if($(this).attr("id")==="down"){
+          $(this).attr('src',"assets/images/blueLight1.png");
+          $("#zones").delay(500).animate({top: '-100%'},{ duration: 500}).fadeOut().animate({top: '0%'},{ duration: 100});
+        }
       setTimeout(newRound,1000);
     }else{
       point--;
       console.log(point);
-      $(this).effect('shake');
+      // $(this).effect('shake');
     }
   });
+
+
   $points.text("points: "+point);
-$('body').append($points);
+  $('body').append($points);
 }
+
+
+function flicker(){
+  if($lights.attr('src')===light1 ){
+        $lights.attr('src',light2);
+    } else{
+      $lights.attr('src',light1);
+    }
+}
+
+
 
 
 function newRound(){
+  $("#zones").fadeIn();
+flickInterval = setInterval(flicker,500);
 correctGuess = guess[Math.floor(Math.random() * guess.length)];
 console.log(correctGuess);
+  // setInterval(hint,Math.floor(Math.random() * 3000));
+
+
+
+
 }
+
+
 
 function hint(){
   let randomVoice = voices[Math.floor(Math.random() * voices.length)];
